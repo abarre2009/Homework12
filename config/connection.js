9177645310b641a4da8382864bb5c24efc8e3497
abdul Barre <abarre2009@gmail.com>
2020-03-14 17:15:37 -0500

    push to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,19 @@
-var mysql = require ("mysql")
+var mysql = require("mysql");
 
-var connection = mysql.createConnection({
- host: "localhost", 
- port: 3306, 
-user: "root", 
-password: "Eagles#27",
-database: "burgers_db"
-
-
-
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Eagles#27",
+    database: "burgers_db"
+  });
+}
+connection.connect(function(err) {
+  if (err) throw err;
+  else console.log("databaseconected");
 });
-
-connection.connect(function(err){
-
-    if(err)throw err 
-    else console.log("databaseconected")
-
-} )
 module.exports = connection;
